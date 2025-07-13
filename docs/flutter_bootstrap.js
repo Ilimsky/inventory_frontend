df89412231664e6e7094bb8402ce46db198eba3c
Ilimsky <ilim.sarykbaev@gmail.com>
2025-07-13 11:35:50 +0600

    added websocket 13.07.25 11:35

--- docs/flutter_bootstrap.js
+++ docs/flutter_bootstrap.js
@@ -39,6 +39,6 @@ _flutter.buildConfig = {"engineRevision":"109150893958777c8f2215f6cfd3e89e984e8d
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2378410261"
+    serviceWorkerVersion: "3225150975"
   }
 });
