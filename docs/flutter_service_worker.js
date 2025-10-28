fdcabf51972bf85ca24df37bad0a9f4dafe30279
Ilimsky <ilim.sarykbaev@gmail.com>
2025-10-29 05:59:03 +0600

    29.10.2025 05:58

--- docs/flutter_service_worker.js
+++ docs/flutter_service_worker.js
@@ -25,7 +25,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "43338579a56b579f7cf04d7519e31f90
 "canvaskit/skwasm.worker.js": "89990e8c92bcb123999aa81f7e203b1c",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "83d881c1dbb6d6bcd6b42e274605b69c",
-"flutter_bootstrap.js": "006d62099d34b04235f67e14eb7cc4f0",
+"flutter_bootstrap.js": "14175b74385eabd3ed023068a0eae564",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
